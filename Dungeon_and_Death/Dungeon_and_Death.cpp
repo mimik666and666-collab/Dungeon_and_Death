2c5a3a8b58d0aa09218b2dc3e34eb12f3a05a6f2
Treezerook <mimik666and666@gmail.com>
2026-03-16 17:22:01 +0700

    little patch all classes

--- Dungeon_and_Death/Dungeon_and_Death.cpp
+++ Dungeon_and_Death/Dungeon_and_Death.cpp
@@ -1,20 +1,58 @@
-﻿// Dungeon_and_Death.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+#include <vector>
+using namespace std;
 
-#include <iostream>
-
-int main()
+class character
 {
-    std::cout << "Hello World!\n";
-}
+private:
+    string Name;
+    int Age;
+    int Race;
+    string color;
+    string portrait;
+    int Class;
+    int Weapon;
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+public:
+    virtual void Speak(string text) = 0;
+    
+};
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+class Choice
+{
+public:
+    Scene nextScene;
+
+private:
+    string text;
+};
+
+
+
+
+class Scene
+{
+
+private:
+    string Title;
+    string ASCIIBackground;
+    vector<Choice> Choice;
+
+public:
+   
+};
+class Core
+{
+private:
+    Core()
+    {};
+    ~Core()
+    {};
+
+public:
+    Core();
+    ~Core();
+
+};
