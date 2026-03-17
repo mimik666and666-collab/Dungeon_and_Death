a4aaf453e233bd54063e3b143a78a5c7a0256a00
Treezerook <mimik666and666@gmail.com>
2026-03-17 18:27:07 +0700

    mimi

--- Dungeon_and_Death/Dungeon_and_Death.cpp
+++ Dungeon_and_Death/Dungeon_and_Death.cpp
@@ -10,14 +10,40 @@ private:
     string Name;
     int Age;
     int Race;
-    string color;
-    string portrait;
+    string Color;
+    string Portrait;
     int Class;
     int Weapon;
-
+    character(){}
 public:
     virtual void Speak(string text) = 0;
-    
+    character(string name, int age, int race, string Color, string Portrait, int Class, int weapon)
+    {
+        this->Name;
+        this->Age;
+        this->Class;
+        this->Color;
+        this->Portrait;
+        this->Race;
+        this->Weapon;
+
+    }
+    void setterName(string naming) { naming = Name; }
+    void setterAge(int aging) { aging = Age; }
+    void setterRace(int Racing) { Racing = Race; }
+    void setterColor(string Coloring) { Coloring = Color; }
+    void setterPortrait(string portrating) { portrating = Portrait; }
+    void setterClass(int Classing) { Classing = Class; }
+    void setterWeapon(int weaponing) { weaponing = Weapon; }
+
+    void getterName(string nam) { Name = nam; }
+    void getterAge(int ag) { Age = ag; }
+    void getterRace(int rac) { Race = rac; }
+    void getterColor(string col) { Color = col; }
+    void getterPortrait(string portra) { Portrait = portra; }
+    void getterClass(int clas) { Class = clas; }
+    void getterWeapon(int weap) { Weapon = weap; }
+
 };
 
 class Choice
@@ -43,6 +69,11 @@ private:
 public:
    
 };
+
+
+
+
+
 class Core
 {
 private:
@@ -52,7 +83,7 @@ private:
     {};
 
 public:
-    Core();
-    ~Core();
+    Core() {};
+    ~Core() {};
 
 };
